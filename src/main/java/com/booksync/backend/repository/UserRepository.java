bc9f2aad1bb5c4f062f093a31ea808622a382be7
mikaellecam <lecam.mikael@gmail.com>
2025-01-09 14:19:09 +0100

    User model

---
+++ src/main/java/com/booksync/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.booksync.backend.repository;
+
+import com.booksync.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
