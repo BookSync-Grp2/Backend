88cd65015585f6d034697bb5913f2986fe0b994a
mikaellecam <lecam.mikael@gmail.com>
2025-01-20 00:32:55 +0100

    added a coverUrl field

--- src/main/java/com/booksync/backend/model/Book.java
+++ src/main/java/com/booksync/backend/model/Book.java
@@ -21,6 +21,9 @@ public class Book {
     private String ISBN;
 
     @Column(nullable = false)
+    private String coverUrl;
+
+    @Column(nullable = false)
     private int publishedYear;
 
     @Column(nullable = false)
@@ -38,6 +41,9 @@ public class Book {
     public String getISBN() {
         return ISBN;
     }
+    public String getCoverUrl() {
+        return coverUrl;
+    }
     public int getPublishedYear() {
         return publishedYear;
     }
@@ -54,10 +60,26 @@ public class Book {
     public void setISBN(String ISBN) {
         this.ISBN = ISBN;
     }
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
     public void setPublishedYear(int publishedYear) {
         this.publishedYear = publishedYear;
     }
     public void setAvailable(boolean available) {
         isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", ISBN='" + ISBN + '\'' +
+                ", coverUrl='" + coverUrl + '\'' +
+                ", publishedYear=" + publishedYear +
+                ", isAvailable=" + isAvailable +
+                '}';
     }
 }
