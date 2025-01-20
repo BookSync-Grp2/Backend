00796c21b95ae6de72b6bae29b5f5e3613e44513
mikaellecam <lecam.mikael@gmail.com>
2025-01-20 22:16:09 +0100

    removed unused method

--- src/main/java/com/booksync/backend/repository/LoanRepository.java
+++ src/main/java/com/booksync/backend/repository/LoanRepository.java
@@ -2,22 +2,11 @@ package com.booksync.backend.repository;
 
 import com.booksync.backend.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
     List<Loan> findByUserId(Long userId);
-
-    @Query("SELECT l " +
-            "FROM Loan l " +
-            "WHERE l.userId = :userId " +
-            "AND l.isReturned = false " +
-            "AND l.loanEndDate >= :currentDate " +
-            "ORDER BY l.loanEndDate ASC")
-    List<Loan> findCurrentLoansByUserId(@Param("userId") Long userId, @Param("currentDate") Date currentDate);
 }
